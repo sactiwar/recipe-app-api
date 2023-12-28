17c179313dc9f349fc6f856bd590bd2b7c148b8f
sactiwar <sactiwar@gmail.com>
2023-12-28 23:47:54 +0530

    Admin.py

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin # noqa
+"""django admin customization"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+# from django.contrib.auth.models import User
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define admin page for user"""
+
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
--- app/core/models.py
+++ app/core/models.py
@@ -17,7 +17,7 @@ class UserManager(BaseUserManager):
             raise ValueError("User must have email address")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
@@ -26,7 +26,7 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 

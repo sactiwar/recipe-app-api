0a231523ffad28219664c21135f46d3e96f894d4
sactiwar <sactiwar@gmail.com>
2024-01-07 19:09:49 +0530

    Admin.py

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,7 +1,7 @@
 """django admin customization"""
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 from core import models
 
